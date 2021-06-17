8bc05f8ebd92978a99936515bd173a1f14c9b354
reviewfromdanamulyana <86062545+reviewfromdanamulyana@users.noreply.github.com>
2021-06-17 22:27:14 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-import * as THREE from 'https://cdn.skypack.dev/three@0.129.0';
-import * from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.129.0/build/three.module.js';
+import { GLTFLoader } from "https://cdn.jsdelivr.net/npm/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
 const scene = new THREE.Scene();
 const world = document.querySelector('#world-render')
